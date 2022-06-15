51bcb1dd5d77df15e1651085f72619957f19a303
lilpastel <quiz@kakao.com>
2022-06-15 17:28:32 +0900

    vim-plugin added

--- shell/zsh_tool.sh
+++ shell/zsh_tool.sh
@@ -2,3 +2,6 @@
 
 # auto-suggetions install
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# vim-plugin
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \\n    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
